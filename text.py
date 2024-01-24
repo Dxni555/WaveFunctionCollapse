82532016c1cb92c7295940d19e7330ed204b221e
Daniel Mattes <mattes.daniel.md@gmail.com>
2024-01-24 17:34:15 +0100

    expend the algorythm for M,C,S - the code is shit but still working:)

--- text.py
+++ text.py
@@ -27,6 +27,7 @@ place_element_index = 1 # top, bottom
 place_element_index2 = 15 # left, right
 place_pick = rand.randint(1, 4) # decides random int to place top, bottom, left or right
 
+# algorythm
 for i in range(len(sign_list)):
     for j in range(len(elements)):
         if sign_list[i] == elements[j]:
@@ -38,11 +39,71 @@ for i in range(len(sign_list)):
 
                 if place_pick == 1:
                     sign_list[i+place_element_index] = temp_L
+                    break
                 elif place_pick == 2:
                     sign_list[i-place_element_index] = temp_L
+                    break
                 elif place_pick == 3:
                     sign_list[i-place_element_index2] = temp_L
+                    break
                 else:
                     sign_list[i+place_element_index2] = temp_L
-                    
-                create_field()
+                    break
+            
+            elif elements[j] == "M":
+                print("\nElement:", elements[j], "an Stelle", i)
+                next_to_M_elements = "L"
+                temp_M = next_to_M_elements
+
+                if place_pick == 1:
+                    sign_list[i+place_element_index] = temp_M
+                    break
+                elif place_pick == 2:
+                    sign_list[i-place_element_index] = temp_M
+                    break
+                elif place_pick == 3:
+                    sign_list[i-place_element_index2] = temp_M
+                    break
+                else:
+                    sign_list[i+place_element_index2] = temp_M
+                    break
+            
+            elif elements[j] == "C":
+                print("\nElement:", elements[j], "an Stelle", i)
+                next_to_C_elements = ["L", "S"]
+                pick_random_C_element = rand.randint(0, 1)
+                temp_C = next_to_C_elements[pick_random_C_element]
+
+                if place_pick == 1:
+                    sign_list[i+place_element_index] = temp_C
+                    break
+                elif place_pick == 2:
+                    sign_list[i-place_element_index] = temp_C
+                    break
+                elif place_pick == 3:
+                    sign_list[i-place_element_index2] = temp_C
+                    break
+                else:
+                    sign_list[i+place_element_index2] = temp_C
+                    break
+            
+            elif elements[j] == "S":
+                print("\nElement:", elements[j], "an Stelle", i)
+                next_to_S_elements = "S"
+                temp_S = next_to_S_elements
+
+                if place_pick == 1:
+                    sign_list[i+place_element_index] = temp_S
+                    break
+                elif place_pick == 2:
+                    sign_list[i-place_element_index] = temp_S
+                    break
+                elif place_pick == 3:
+                    sign_list[i-place_element_index2] = temp_S
+                    break
+                else:
+                    sign_list[i+place_element_index2] = temp_S
+                    break
+            
+
+create_field()
